0c13a1e9f5b0f8ef313f0984fca1f1c0ae3acd87
cjshallow <cjshallow@gmail.com>
2023-09-27 13:17:06 -0400

    Added Resizing within Container and Centered All Content

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName('gridRows');
 
+
 defaultGrid();
 
 function defaultGrid(){
     createRows(16);
     createCells(16);
+    hoverColor();
 }
 
 
@@ -25,37 +27,38 @@ function createCells(cn) {
     }
 }
 
-const cellColor = document.querySelectorAll(".cell");
 
-cellColor.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.background = "black";
-    });
-});
+function hoverColor() {
+  const cellColor = document.querySelectorAll(".cell");
+  cellColor.forEach((cell) => {
+      cell.addEventListener('mouseover', () => {
+          cell.style.background = "black";
+      });
+  });
+};
 
 
 function changeGrid(){
   rn = window.prompt("Input number of rows", 1);
   cn = window.prompt("Input number of Columns", 1);
 
-
-  container.innerHTML = '';
-  
-  for (i = 0; i < parseInt(rn, 10); i++){
-    let row = document.createElement('div');
-    container.appendChild(row).className = "gridRows";
+  if (cn > 100 || cn < 10 || rn > 100 || rn < 10) {
+    alert("Please choose a number between 10 and 100");
+  } else {
+    container.innerHTML = '';
+    for (i = 0; i < parseInt(rn, 10); i++){
+      let row = document.createElement('div');
+      container.appendChild(row).className = "gridRows";
+    }
+    for (a = 0; a < rows.length; a++) {
+      for (b = 0; b < cn; b++) {
+          let newCell = document.createElement('div');
+          rows[b].appendChild(newCell).className = "cell";
+      }
+    }
   }
-  for (a = 0; a < rows.length; a++) {
-    for (b = 0; b < cn; b++) {
-        let newCell = document.createElement('div');
-        rows[b].appendChild(newCell).className = "cell";
-  }
+  hoverColor();
 }
-}
-
-
-//change the cell size when new grid is made to fit into same pixels of 
-//container//
 
 
 
